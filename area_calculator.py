ccab50d4e2e7b7253986b496e0a65907fa775563
André Braithwaite <andrebwt@gmail.com>
2024-06-12 22:11:56 +0100

    Add source files

---
+++ area_calculator.py
@@ -0,0 +1,10 @@
+# x = int(input("What is side one of the rectangle? "))
+# y = int(input("What is side two of the rectangle? "))
+
+# print("The area of the rectangle is: ")
+# print(x * y)
+
+z = 3
+d = 8
+
+print(type(z))
---
+++ float.py
@@ -0,0 +1,5 @@
+x = 12 /4
+y = 5 + 6.1
+print(f"Type of x is {type(x)}")
+print(f"Value of x is {x}")
+print(f"Type of y is {type(y)}")
